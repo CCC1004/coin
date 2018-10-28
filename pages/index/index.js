e22cc870e369ded8c6e99adfbc0a7e7cb21d45cd
CCC1004 <15810586466@163.com>
2018-10-28 23:13:07 +0800

    20181028

---
+++ pages/index/index.js
@@ -0,0 +1,36 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: 'true',
+    autoplay: 'true',
+    interval: '3000',
+    duration: '1000',
+    //轮播图
+    imgUrls: [
+      '../../images/1.jpg',
+      '../../images/2.jpg',
+      '../../images/4.jpg'
+    ],
+    //集合图片
+    selectsImgs: [
+      '../../images/9.jpg',
+      '../../images/10.jpg',
+      '../../images/13.jpg',
+      '../../images/14.jpg'
+    ],
+    //列表图片
+    listsImgs: [
+      '../../images/3.jpg',
+      '../../images/7.jpg',
+      '../../images/8.jpg',
+      '../../images/15.jpg'
+    ],
+    // 头像图片
+    headImg: '../../images/timg.jpg'
+
+  },
+
+})
