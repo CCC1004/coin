1e75ca47f04e4e95ac62d80439d6f3e8e7b932e2
CCC1004 <15810586466@163.com>
2018-12-10 00:48:42 +0800

    20181210-00

--- pages/index/kind/kind.js
+++ pages/index/kind/kind.js
@@ -1,14 +1,17 @@
-// pages/index/detail/detail.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    //图片资源访问IP
+    urlInfo: null,
     //分类名称
     flName: null,
-     //分类详情列表数据 
-    flList: null,
+     //所属分类的品种列表数据 
+    kindData: null,
 
   },
 
@@ -18,22 +21,33 @@ Page({
   onLoad: function (options) {
     var that = this;
 
-    var guid = options.guid;
+    //获取分类id和分类名称
+    var flid = options.guid;
     var name = options.name;
     that.setData({
       flName: name,
     })
 
+    //设置url
+    this.setData({
+      urlInfo: app.globalData.coin_url,
+    })
+
+    //获取所属分类的品种列表
+    this.getKindList(this, flid)
+
+  },
+
+  getKindList: function (that, flid) {
     wx.request({
-      url: app.globalData.coin_url + 'index/searchDetailByGuid',
-      data: { guid: guid},
+      url: app.globalData.coin_url + 'index/searchKindListByflid',
+      data: { 'flid': flid },
       success: function (res) {
         that.setData({
-          flList: res.data.data,
+          kindData: res.data.data,
         })
       }
     })
-
   },
 
   /**
