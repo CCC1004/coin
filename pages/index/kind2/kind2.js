e77827729edfc70eacb8e0bd0a06aa60901ad66d
CCC1004 <15810586466@163.com>
2018-12-12 00:01:57 +0800

    20181211

--- pages/index/kind2/kind2.js
+++ pages/index/kind2/kind2.js
@@ -11,8 +11,7 @@ Page({
     //分类名称
     flName: null,
     //所属分类的品种列表数据 
-    kindData: null,
-
+    kindData: null,  
   },
 
   /**
@@ -50,52 +49,7 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
